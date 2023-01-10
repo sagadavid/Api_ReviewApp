81f984bcdc776cc8c0f28aa29ef226d195b7d0ce
sagadavid <epostsaga@gmail.com>
2023-01-10 04:06:34 +0100

    -iownerrepository and ownerrepository created

---
+++ Api_ReviewApp/Interfaces/IOwnerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Api_ReviewApp.Models;
+
+namespace Api_ReviewApp.Interfaces
+{
+    public interface IOwnerRepository
+    {
+        ICollection<Owner> GetOwners();
+        Owner GetOwner(int ownerId);
+        ICollection<Owner> GetOwnerByPokemonId(int pokeId);
+        ICollection<Pokemon> GetPokemonByOwnerId(int ownerId);
+        bool OwnerExists(int ownerId);
+        
+    }
+}
---
+++ Api_ReviewApp/Repository/OwnerRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Api_ReviewApp.Repository
+{
+    public class OwnerRepository
+    {
+    }
+}
